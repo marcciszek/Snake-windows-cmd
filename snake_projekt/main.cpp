2d05b14772b0d750306878fca29912d4c12ff790
Marcin Ciszek <marciszek0@gmail.com>
2020-02-04 21:18:38 +0100

    nowe mapy i zapis punktow

--- snake_projekt/main.cpp
+++ snake_projekt/main.cpp
@@ -3,34 +3,38 @@
 #include <random>
 #include <string>
 #include <limits>
+#include <fstream>
 
 using namespace std;
 
 void petla_gry(int koniec_poziomu, int wysokosc, int szerokosc, int ksztalt_planszy, int predkosc, bool& koniec_gry,int tryb);
+void gra_snake();
 void menu_glowne(int& szerokosc, int& wysokosc, int& predkosc, int& ksztalt_planszy, bool& koniec_gry, bool& koniec_programu, int& tryb);
 void pkt_wyswietl();
 void wybierz_predkosc(int &predkosc);
-int wybierz_plansze();
+int  wybierz_plansze();
 void wybierz_rozmiar(int& szerokosc, int& wysokosc);
 void wypisz_informacje();
 
 void ustawienia_okna();
+void wczytaj_wynik();
+void zapisz_wynik();
 
 void ustaw_weza(int szer, int wys,int **plansza);
 void ustaw_cel(int szer, int wys,bool& ustawiony, int** plansza);
 void sterowanie(int& kierunek,bool& koniec);
 void ruch_weza(int kierunek);
-int sprawdz_weza(int& dlugosc,bool& koniec,bool& ustawiony, int** plansza, int tryb);
-void rysuj2(int szer, int wys, int** plansza,int tryb,int zebrano);
+int  sprawdz_weza(int& dlugosc,bool& koniec,bool& ustawiony, int** plansza, int tryb);
+void rysuj2(int szer, int wys, int** plansza,int tryb,int zebrano, int koniec_poziomu);
 void rysuj_weza(int dlugosc, int** plansza);
 void plansza_1_ramka(int szer, int wys, int** plansza);
 void plansza_2_ramka_linie(int szer, int wys, int** plansza);
+void plansza_3_kwadrat(int szer, int wys, int** plansza);
+void plansza_4_U(int szer, int wys, int** plansza);
 void snake_napis();
 void clearScreen_nadpisz();
 void clearScreen();
 void ShowConsoleCursor(bool showFlag);
-
-//wpisany znak po zakonczeniu gry
 
 int waz[2500][2]{};//0=x 1=y
 int waz_poprzedni[2500][2]{}; 
@@ -49,112 +53,10 @@ int kroki{};
 int main()
 {
 	ustawienia_okna();
-	//domyslne ustawienia
-	int wysokosc = 30;
-	int szerokosc = 30;
-	int predkosc{ 55 };
-	int ksztalt_planszy = 1;
-	int tryb_gry = 1; //1 normalna, 2 przygoda
+	wczytaj_wynik();
+	gra_snake();
+	zapisz_wynik();
 
-	while (true)
-	{
-		bool koniec_gry = true;
-		bool koniec_programu = false;
-
-		menu_glowne(szerokosc, wysokosc, predkosc, ksztalt_planszy, koniec_gry, koniec_programu,tryb_gry);
-		
-		if (tryb_gry == 1 and koniec_programu == false)
-		{
-			pkt_ostatnio = 0;
-			petla_gry(-1, wysokosc, szerokosc, ksztalt_planszy, predkosc, koniec_gry, tryb_gry);
-		}
-		else if (tryb_gry == 2 and koniec_programu == false)
-		{
-			pkt_przygoda_ostatnio = 0;
-
-			if (koniec_gry == false)
-			{
-				clearScreen();
-				cout << endl << endl << "  Poziom 1";
-				Sleep(3000);
-				petla_gry(20, 50, 50, 1, 55, koniec_gry, tryb_gry);
-			}
-			if (koniec_gry == false)
-			{
-				clearScreen();
-				cout << endl << endl << "  Poziom 2";
-				Sleep(3000);
-				pkt_bonus++;
-				petla_gry(20, 50, 50, 2, 55, koniec_gry, tryb_gry);
-			}
-			if (koniec_gry == false)
-			{
-				clearScreen();
-				cout << endl << endl << "  Poziom 3";
-				Sleep(3000);
-				pkt_bonus++;
-				petla_gry(20, 50, 50, 1, 45, koniec_gry, tryb_gry);
-			}
-			if (koniec_gry == false)
-			{
-				clearScreen();
-				cout << endl << endl << "  Poziom 4";
-				Sleep(3000);
-				pkt_bonus++;
-				petla_gry(20, 50, 50, 2, 45, koniec_gry, tryb_gry);
-			}
-			if (koniec_gry == false)
-			{
-				clearScreen();
-				cout << endl << endl << "  Poziom 5";
-				Sleep(3000);
-				pkt_bonus++;
-				petla_gry(20, 40, 40, 1, 45, koniec_gry, tryb_gry);
-			}
-			if (koniec_gry == false)
-			{
-				clearScreen();
-				cout << endl << endl << "  Poziom 6";
-				Sleep(3000);
-				pkt_bonus++;
-				petla_gry(20, 40, 40, 2, 45, koniec_gry, tryb_gry);
-			}
-			if (koniec_gry == false)
-			{
-				clearScreen();
-				cout << endl << endl << "  Poziom 7";
-				Sleep(3000);
-				pkt_bonus++;
-				petla_gry(15, 40, 40, 1, 40, koniec_gry, tryb_gry);
-			}
-			if (koniec_gry == false)
-			{
-				clearScreen();
-				cout << endl << endl << "  Poziom 8";
-				Sleep(3000);
-				pkt_bonus++;
-				petla_gry(15, 40, 40, 2, 40, koniec_gry, tryb_gry);
-			}
-			if (koniec_gry == false)
-			{
-				clearScreen();
-				cout << endl << endl << "  Poziom 9";
-				Sleep(3000);
-				pkt_bonus++;
-				petla_gry(10, 30, 30, 1, 35, koniec_gry, tryb_gry);
-			}
-			if (koniec_gry == false)
-			{
-				clearScreen();
-				cout << endl << endl << "  Poziom 10";
-				Sleep(3000);
-				pkt_bonus++;
-				petla_gry(10, 30, 30, 2, 35, koniec_gry, tryb_gry);
-			}
-		}
-
-		if (koniec_programu == true)break;
-	}
 }
 
 void petla_gry(int koniec_poziomu,int wysokosc,int szerokosc,int ksztalt_planszy,int predkosc,bool& koniec_gry,int tryb)
@@ -164,7 +66,6 @@ void petla_gry(int koniec_poziomu,int wysokosc,int szerokosc,int ksztalt_planszy
 	bool cel_stawiony = false;
 	int zebrano{};
 	kroki = 0;
-
 
 	clearScreen();
 	ShowConsoleCursor(false);
@@ -177,6 +78,8 @@ void petla_gry(int koniec_poziomu,int wysokosc,int szerokosc,int ksztalt_planszy
 	}
 	if (ksztalt_planszy == 1) plansza_1_ramka(szerokosc, wysokosc, plansza);
 	else if (ksztalt_planszy == 2) plansza_2_ramka_linie(szerokosc, wysokosc, plansza);
+	else if (ksztalt_planszy==3) plansza_3_kwadrat(szerokosc, wysokosc, plansza);
+	else if (ksztalt_planszy==4) plansza_4_U(szerokosc, wysokosc, plansza);
 
 	//petla gra
 	ustaw_weza(szerokosc, wysokosc, plansza);
@@ -187,11 +90,20 @@ void petla_gry(int koniec_poziomu,int wysokosc,int szerokosc,int ksztalt_planszy
 		sterowanie(kierunek, koniec_gry);
 		ruch_weza(kierunek);
 		zebrano+=sprawdz_weza(dlugosc_weza, koniec_gry, cel_stawiony, plansza, tryb);
-		rysuj2(szerokosc, wysokosc, plansza,tryb,zebrano);
+		rysuj2(szerokosc, wysokosc, plansza,tryb,zebrano, koniec_poziomu);
 		Sleep(predkosc);
 		if (kierunek != 0) kroki++;
 		if (zebrano == koniec_poziomu) break;
+		if (koniec_gry)
+		{
+			Sleep(800);
+			FlushConsoleInputBuffer(GetStdHandle(STD_INPUT_HANDLE));
+			if (zebrano == koniec_poziomu) cout << "\n\n  Gratulacje! \n\n  Nacisnij ENTER, aby przejsc do nastepnego poziomu ";
+			else cout << "\n\n  Koniec gry :(\n\n  Nacisnij ENTER, aby przejsc do menu ";
+			getchar();
+		}
 	}
+
 	//sprzatanie
 	ShowConsoleCursor(true);
 	for (int i = 0;i < 1000;i++)
@@ -208,6 +120,117 @@ void petla_gry(int koniec_poziomu,int wysokosc,int szerokosc,int ksztalt_planszy
 	delete[] plansza;
 }
 
+void gra_snake()
+{
+	//domyslne ustawienia
+	int wysokosc = 30;
+	int szerokosc = 30;
+	int predkosc{ 55 };
+	int ksztalt_planszy = 1;
+	int tryb_gry = 1; //1 normalna, 2 przygoda
+	
+	
+	while (true)
+	{
+		bool koniec_gry = true;
+		bool koniec_programu = false;
+
+		menu_glowne(szerokosc, wysokosc, predkosc, ksztalt_planszy, koniec_gry, koniec_programu, tryb_gry);
+
+		if (tryb_gry == 1 and koniec_programu == false)
+		{
+			pkt_ostatnio = 0;
+			petla_gry(-1, wysokosc, szerokosc, ksztalt_planszy, predkosc, koniec_gry, tryb_gry);
+		}
+		else if (tryb_gry == 2 and koniec_programu == false)
+		{
+			pkt_przygoda_ostatnio = 0;
+
+			if (koniec_gry == false)
+			{
+				clearScreen();
+				cout << endl << endl << "  Poziom 1";
+				Sleep(3000);
+				petla_gry(5, 40, 40, 1, 65, koniec_gry, tryb_gry);
+			}
+			if (koniec_gry == false)
+			{
+				clearScreen();
+				cout << endl << endl << "  Poziom 2";
+				Sleep(3000);
+				pkt_bonus++;
+				petla_gry(5, 40, 40, 3, 65, koniec_gry, tryb_gry);
+			}
+			if (koniec_gry == false)
+			{
+				clearScreen();
+				cout << endl << endl << "  Poziom 3";
+				Sleep(3000);
+				pkt_bonus++;
+				petla_gry(5, 40, 40, 4, 65, koniec_gry, tryb_gry);
+			}
+			if (koniec_gry == false)
+			{
+				clearScreen();
+				cout << endl << endl << "  Poziom 4";
+				Sleep(3000);
+				pkt_bonus++;
+				petla_gry(5, 40, 40, 2, 65, koniec_gry, tryb_gry);
+			}
+			if (koniec_gry == false)
+			{
+				clearScreen();
+				cout << endl << endl << "  Poziom 5";
+				Sleep(3000);
+				pkt_bonus++;
+				petla_gry(5, 30, 30, 1, 55, koniec_gry, tryb_gry);
+			}
+			if (koniec_gry == false)
+			{
+				clearScreen();
+				cout << endl << endl << "  Poziom 6";
+				Sleep(3000);
+				pkt_bonus++;
+				petla_gry(5, 30, 30, 4, 55, koniec_gry, tryb_gry);
+			}
+			if (koniec_gry == false)
+			{
+				clearScreen();
+				cout << endl << endl << "  Poziom 7";
+				Sleep(3000);
+				pkt_bonus++;
+				petla_gry(5, 30, 30, 2, 55, koniec_gry, tryb_gry);
+			}
+			if (koniec_gry == false)
+			{
+				clearScreen();
+				cout << endl << endl << "  Poziom 8";
+				Sleep(3000);
+				pkt_bonus++;
+				petla_gry(5, 40, 40, 1, 40, koniec_gry, tryb_gry);
+			}
+			if (koniec_gry == false)
+			{
+				clearScreen();
+				cout << endl << endl << "  Poziom 9";
+				Sleep(3000);
+				pkt_bonus++;
+				petla_gry(5, 40, 40, 4, 40, koniec_gry, tryb_gry);
+			}
+			if (koniec_gry == false)
+			{
+				clearScreen();
+				cout << endl << endl << "  Poziom 10";
+				Sleep(3000);
+				pkt_bonus++;
+				petla_gry(5, 40, 40, 2, 40, koniec_gry, tryb_gry);
+			}
+		}
+
+		if (koniec_programu == true)break;
+	}
+}
+
 void menu_glowne(int& szerokosc,int& wysokosc,int& predkosc,int&ksztalt_planszy,bool &koniec_gry,bool& koniec_programu,int& tryb)
 {
 	int menu{};
@@ -216,14 +239,14 @@ void menu_glowne(int& szerokosc,int& wysokosc,int& predkosc,int&ksztalt_planszy,
 	clearScreen();
 	snake_napis();
 	pkt_wyswietl();
-	cout << "  Wpisz odpowiednia cyfre i nacisnij ENTER" << endl << endl;
-	cout << "  1. Tryb normalny" << endl;
-	cout << "  2. zmien poziom trudnosci" << endl;
-	cout << "  3. zmien rodzaj planszy" << endl;
-	cout << "  4. zmien rozmiar planszy" << endl;
-	cout << "  5. Przygoda" << endl;
-	cout << "  8. Informacje" << endl;
-	cout << "  9. zakoncz gre" << endl << endl;
+	cout <<endl<< "  Wpisz odpowiednia cyfre i nacisnij ENTER" << endl << endl;
+	cout << "  1. Tryb normalny" << endl << endl;
+	cout << "  2. Zmien poziom trudnosci" << endl << endl;
+	cout << "  3. Zmien rodzaj planszy" << endl << endl;
+	cout << "  4. Zmien rozmiar planszy" << endl << endl;
+	cout << "  5. Przygoda" << endl << endl << endl;
+	cout << "  8. Informacje" << endl << endl;
+	cout << "  9. Zakoncz gre" << endl << endl;
 	cout << "  Twoj wybor: ";
 
 		cin >> menu;
@@ -281,21 +304,23 @@ void wybierz_predkosc(int& predkosc)
 		cout << "  Wpisz odpowiednia cyfre i nacisnij ENTER" << endl << endl;
 		cout << "  Dostepne poziomy trudnosci: " << endl << endl;
 		cout << "       predkosc" << endl << endl;
-		cout << "  (1)   wolno               " << endl;
-		cout << "  (2)   normalnie           " << endl;
-		cout << "  (3)   szybko              " << endl;
-		cout << "  (4)   bardzo szybko       " << endl << endl;
+		cout << "  (1)   bardzo wolno        " << endl;
+		cout << "  (2)   wolno               " << endl;
+		cout << "  (3)   normalnie (domyslnie)" << endl;
+		cout << "  (4)   szybko              " << endl;
+		cout << "  (5)   bardzo szybko       " << endl << endl;
 		cout << "  Wybierz poziom trudnosci: ";
 
 		cin >> wybor;
 		cin.clear();
 		cin.ignore((std::numeric_limits<std::streamsize>::max)(), '\n');
 
-	} while (wybor != 1 and wybor != 2 and wybor != 3);
-	if (wybor == 1) predkosc = 70;
-	else if (wybor == 2) predkosc = 55;
-	else if (wybor == 3) predkosc = 40;
-	else if (wybor == 4) predkosc = 25;
+	} while (wybor != 1 and wybor != 2 and wybor != 3 and wybor != 4 and wybor != 5);
+	if (wybor == 1) predkosc = 100;
+	else if (wybor == 2) predkosc = 70;
+	else if (wybor == 3) predkosc = 55;
+	else if (wybor == 4) predkosc = 40;
+	else if (wybor == 5) predkosc = 25;
 }
 
 int wybierz_plansze()
@@ -326,11 +351,31 @@ int wybierz_plansze()
 		cout << "  ***   ***" << endl;
 		cout << "  *       *" << endl;
 		cout << "  *********" << endl << endl;
+		cout << "  Trzecia pierwsza(3)" << endl << endl;
+		cout << "  *********" << endl;
+		cout << "  *       *" << endl;
+		cout << "  *       *" << endl;
+		cout << "  *  ***  *" << endl;
+		cout << "  *  ***  *" << endl;
+		cout << "  *  ***  *" << endl;
+		cout << "  *       *" << endl;
+		cout << "  *       *" << endl;
+		cout << "  *********" << endl << endl;
+		cout << "  Czwarta pierwsza(4)" << endl << endl;
+		cout << "  *********" << endl;
+		cout << "  *       *" << endl;
+		cout << "  *       *" << endl;
+		cout << "  *  ***  *" << endl;
+		cout << "  *  * *  *" << endl;
+		cout << "  *  * *  *" << endl;
+		cout << "  *       *" << endl;
+		cout << "  *       *" << endl;
+		cout << "  *********" << endl << endl;
 		cout << "  Wybierz plansze: ";
 		cin >> wybor;
 		cin.clear();
 		cin.ignore((std::numeric_limits<std::streamsize>::max)(), '\n');
-	} while (wybor != 1 and wybor != 2);
+	} while (wybor != 1 and wybor != 2 and wybor != 3 and wybor != 4);
 	return wybor;
 }
 
@@ -382,6 +427,7 @@ void wypisz_informacje()
 		cout << "  " <<(char)0x4F << "  To jest glowa Twojego weza" << endl << endl;
 		cout << "  " << (char)0xCF << "  To jest element twojego ogona, staraj sie go nie zjesc" << endl << endl;
 		cout << "  " << (char)0x24 << "  To sa dolary, kazdy lubi dolary, zbieraj je" << endl << endl;
+		cout << "  Klawisz E pozwala przerwac gre" << endl << endl;
 		cout << endl << "  Wpisz 1 aby powrocic: ";
 		cin >> koniec;
 		cin.clear();
@@ -416,6 +462,27 @@ void ustawienia_okna()
 	SetConsoleScreenBufferSize(konsola_out, rozmiar);
 	MoveWindow(konsola, rect.left, rect.top, rect.right, rect.bottom, TRUE);
 	ShowWindow(konsola, SW_MAXIMIZE);
+}
+
+void wczytaj_wynik()
+{
+	ifstream plik("punkty.txt");
+	if (plik.is_open() == true)
+	{
+		plik >> pkt_maks;
+		plik >> pkt_przygoda_maks;
+	}
+	plik.close();
+}
+void zapisz_wynik()
+{
+	ofstream plik("punkty.txt");
+	if (plik.is_open() == true)
+	{
+		plik << pkt_maks << endl;
+		plik << pkt_przygoda_maks;
+		plik.close();
+	}
 }
 // ***********************************************************************************************************
 
@@ -501,7 +568,7 @@ int sprawdz_weza(int &dlugosc, bool& koniec,bool& ustawiony, int** plansza, int 
 	return 0;
 }
 
-void rysuj2(int szer, int wys, int** plansza,int tryb,int zebrano) //wykorzystanie string duzo przyspiesza wypisywnie
+void rysuj2(int szer, int wys, int** plansza,int tryb,int zebrano, int koniec_poziomu) //wykorzystanie string duzo przyspiesza wypisywnie
 {
 	string linia;
 	linia += "\n \n";
@@ -527,7 +594,7 @@ void rysuj2(int szer, int wys, int** plansza,int tryb,int zebrano) //wykorzystan
 	}
 	else if (tryb == 2)
 	{
-		linia += "\n  Zebrano: " + to_string(zebrano) + "$";
+		linia += "\n  Zebrano: " + to_string(zebrano) + "$ ("+to_string(koniec_poziomu)+")";
 		linia += "\n  Punkty: " + to_string(pkt_przygoda_ostatnio);
 		linia += "\n  Liczba krokow: " + to_string(kroki);
 	}
@@ -580,6 +647,46 @@ void plansza_2_ramka_linie(int szer, int wys, int** plansza)
 			if (i == wys / 2 and (j < szer / 3 or j >= szer - (szer / 3))) plansza[i][j] = 1;
 			else if (i == wys / 4 and (j < szer / 3 or j >= szer - (szer / 3))) plansza[i][j] = 1;
 			else if (i == wys-(wys/4) and (j < szer / 3 or j >= szer - (szer / 3))) plansza[i][j] = 1;
+		}
+	}
+}
+
+void plansza_3_kwadrat(int szer, int wys, int** plansza)
+{
+	for (int i = 0; i < wys; i++)
+	{
+		for (int j = 0; j < szer; j++)
+		{
+			if (i == 0) plansza[i][j] = 1;
+			else if (i == (wys - 1)) plansza[i][j] = 1;
+			else if (j == 0) plansza[i][j] = 1;
+			else if (j == (szer - 1)) plansza[i][j] = 1;
+
+			if (i >= (wys / 2 - (wys / 6)) and i <= (wys / 2 + (wys / 6)))
+				if (j >= (wys / 2 - wys / 6) and j <= (wys / 2 + wys / 6))
+					plansza[i][j] = 1;
+		}
+	}
+}
+
+void plansza_4_U(int szer, int wys, int** plansza)
+{
+	for (int i = 0; i < wys; i++)
+	{
+		for (int j = 0; j < szer; j++)
+		{
+			if (i == 0) plansza[i][j] = 1;
+			else if (i == (wys - 1)) plansza[i][j] = 1;
+			else if (j == 0) plansza[i][j] = 1;
+			else if (j == (szer - 1)) plansza[i][j] = 1;
+
+			if (i == (wys / 2 - (wys / 4))) 
+				if (j >= (wys / 2 - wys / 6) and j <= (wys / 2 + wys / 6))
+					plansza[i][j] = 1;
+			if (i >= (wys / 2 - wys / 4) and i <= (wys / 2 + wys / 6))
+				if (j == (wys / 2 - wys / 6) or j == (wys / 2 + wys / 6))
+					plansza[i][j] = 1;
+
 		}
 	}
 }
